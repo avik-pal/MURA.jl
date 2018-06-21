2b434e25ff3a78c97c35c2508cf87d13bf727118
Avik Pal <avikpal@iitk.ac.in>
2018-06-21 14:07:07 +0530

    Add missing layer

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # MURA_Densenet
+
 Densenet on the MURA (musculoskeletal radiographs) Dataset using Flux
+
+***This Project is under Development***
--- examples/cifar10_densenet_bc.jl
+++ examples/cifar10_densenet_bc.jl
@@ -5,7 +5,7 @@ using Base.Iterators: partition
 using BSON: @save
 
 struct Bottleneck
-    layer
+  layer
 end
 
 Flux.treelike(Bottleneck)
@@ -23,34 +23,35 @@ Transition(chs::Pair{<:Int, <:Int}) = Chain(BatchNorm(chs[1], relu),
                                             x -> meanpool(x, (2, 2)))
 
 function _make_dense_layers(block, in_planes, growth_rate, nblock)
-    local layers = []
-    for i in 1:nblock
-        push!(layers, block(in_planes, growth_rate))
-        in_planes += growth_rate
-    end
-    Chain(layers...)
+  local layers = []
+  for i in 1:nblock
+    push!(layers, block(in_planes, growth_rate))
+    in_planes += growth_rate
+  end
+  Chain(layers...)
 end
 
 function DenseNet(nblocks; block = Bottleneck, growth_rate = 12, reduction = 0.5, num_classes = 10)
-    num_planes = 2growth_rate
-    layers = []
-    push!(layers, Conv((3, 3), 3=>num_planes, pad = (1, 1)))
+  num_planes = 2growth_rate
+  layers = []
+  push!(layers, Conv((3, 3), 3=>num_planes, pad = (1, 1)))
+  push!(layers, BatchNorm(num_planes, relu))
 
-    for i in 1:3
-        push!(layers, _make_dense_layers(block, num_planes, growth_rate, nblocks[i]))
-        num_planes += nblocks[i] * growth_rate
-        out_planes = Int(floor(num_planes * reduction))
-        push!(layers, Transition(num_planes=>out_planes))
-        num_planes = out_planes
-    end
+  for i in 1:3
+    push!(layers, _make_dense_layers(block, num_planes, growth_rate, nblocks[i]))
+    num_planes += nblocks[i] * growth_rate
+    out_planes = Int(floor(num_planes * reduction))
+    push!(layers, Transition(num_planes=>out_planes))
+    num_planes = out_planes
+  end
 
-    push!(layers, _make_dense_layers(block, num_planes, growth_rate, nblocks[4]))
-    num_planes += nblocks[4] * growth_rate
-    push!(layers, BatchNorm(num_planes, relu))
+  push!(layers, _make_dense_layers(block, num_planes, growth_rate, nblocks[4]))
+  num_planes += nblocks[4] * growth_rate
+  push!(layers, BatchNorm(num_planes, relu))
 
-    Chain(layers..., x -> meanpool(x, (4, 4)),
-          x -> reshape(x, :, size(x, 4)),
-          Dense(num_planes, num_classes), softmax)
+  Chain(layers..., x -> meanpool(x, (4, 4)),
+        x -> reshape(x, :, size(x, 4)),
+        Dense(num_planes, num_classes), softmax)
 end
 
 densenet = DenseNet([6, 12, 24, 16]) |> gpu
@@ -72,14 +73,14 @@ accuracy(x, y) = mean(argmax(x, 1:10) .== argmax(y, 1:10))
 opt = ADAM(params(densenet))
 
 @epochs 5 begin
-    for data in train
-        forward = (densenet(data[1] |> gpu), data[2] |> gpu)
-        l = loss(forward[1], forward[2])
-        println("Loss = $l and Accuracy = $(accuracy(forward[1], forward[2]))")
-        Flux.back!(l)
-        opt()
-    end
-    densenet = densenet |> cpu
-    @save "cifar10_checkpoint.bson" densenet
-    densenet = densenet |> gpu
+  for data in train
+    forward = (densenet(data[1] |> gpu), data[2] |> gpu)
+    l = loss(forward[1], forward[2])
+    println("Loss = $l and Accuracy = $(accuracy(forward[1], forward[2]))")
+    Flux.back!(l)
+    opt()
+  end
+  densenet = densenet |> cpu
+  @save "cifar10_checkpoint.bson" densenet
+  densenet = densenet |> gpu
 end
