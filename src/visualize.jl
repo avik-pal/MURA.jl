bca168c5dfbdd471eb92068dc9cf2effa3878905
Avik Pal <avikpal@iitk.ac.in>
2018-07-02 17:02:42 +0530

    Fixes and working version

--- src/visualize.jl
+++ src/visualize.jl
@@ -24,19 +24,23 @@ end
 
 im2arr_rgb(img) = permutedims(float.(channelview(imresize(img, (224, 224)))), (3, 2, 1))
 
-#-----------------Vanilla BackPropagation-----------------
+function change_activation!(model::Chain, activation)
+  for (i, l) in enumerate(model.layers)
+    if typeof(l) <: Dense
+      model.layers[i] = Dense(l.W, l.b, activation)
+    elseif typeof(l) <: Conv
+      model.layers[i] = Conv(activation, l.weight, l.bias, l.stride, l.pad, l.dilation)
+    elseif typeof(l) <: BatchNorm
+      model.layers[i] = BatchNorm(activation, l.β, l.γ, l.μ, l.σ, l.ϵ, l.momentum, l.active)
+    end
+  end
+end
 
-module VanillaBackprop
-
-using ..get_topk, ..one_hot_encode
-using ..Tracker, ..data, ..Flux
-using Flux: relu
+#--------------------BackPropagation----------------------
 
 struct Backprop
   model::Chain
 end
-
-create(model) = Backprop((model.layers[1:end-1]) |> gpu)
 
 function (m::Backprop)(img, top = 1)
   if !Tracker.istracked(img)
@@ -54,78 +58,34 @@ function (m::Backprop)(img, top = 1)
   grads
 end
 
-end
+#-----------------Vanilla BackPropagation-----------------
+
+VanillaBackprop(model::Chain) = Backprop(model[1:end-1] |> gpu)
 
 #------------------Guided BackPropagation-----------------
 
-module GuidedBackprop
+guided_relu1(x) = max.(zero(x), x)
 
-using ..get_topk, ..one_hot_encode
-using ..Tracker, ..data, ..Flux
-using Flux: relu
+guided_relu1(x::TrackedArray) = Tracker.track(guided_relu1, x)
 
-relu(x::TrackedReal) = Tracker.track(relu, x)
+Tracker.back(::typeof(guided_relu1), Δ, x) = Tracker.@back(x, Int.(x .> zero(x)) .* max.(zero(Δ), Δ))
 
-Tracker.back(::typeof(relu), Δ, x) = Tracker.@back(x, Int(x > zero(x)) * max(zero(Δ), Δ))
-
-struct Backprop
-  model::Chain
-end
-
-create(model) = Backprop((model.layers[1:end-1]) |> gpu)
-
-function (m::Backprop)(img, top = 1)
-  if !Tracker.istracked(img)
-    img = param(img) |> gpu
-  end
-  preds = m.model(img)
-  probs = softmax(preds)
-  prob, inds = get_topk(data(probs), top)
-  grads = []
-  for (i, idx) in enumerate(inds)
-    Flux.back!(preds, one_hot_encode(preds, idx))
-    push!(grads, (clamp.(img.grad |> cpu, 0.0, 1.0), prob[i], idx))
-    img.grad .= 0.0
-  end
-  grads
-end
-
+function GuidedBackprop(model::Chain)
+  change_activation!(model, guided_relu1)
+  Backprop(model[1:end-1] |> gpu)
 end
 
 #---------------------Deconvolution-----------------------
 
-module Deconv
+guided_relu2(x) = max.(zero(x), x)
 
-using ..get_topk, ..one_hot_encode
-using ..Tracker, ..data, ..Flux
-using Flux: relu
+guided_relu2(x::TrackedArray) = Tracker.track(guided_relu2, x)
 
-relu(x::TrackedReal) = Tracker.track(relu, x)
+Tracker.back(::typeof(guided_relu2), Δ, x) = Tracker.@back(x, max(zero(Δ), Δ))
 
-Tracker.back(::typeof(relu), Δ, x) = Tracker.@back(x, max(zero(Δ), Δ))
-
-struct Backprop
-  model::Chain
-end
-
-create(model) = Backprop((model.layers[1:end-1]) |> gpu)
-
-function (m::Backprop)(img, top = 1)
-  if !Tracker.istracked(img)
-    img = param(img) |> gpu
-  end
-  preds = m.model(img)
-  probs = softmax(preds)
-  prob, inds = get_topk(data(probs), top)
-  grads = []
-  for (i, idx) in enumerate(inds)
-    Flux.back!(preds, one_hot_encode(preds, idx))
-    push!(grads, (clamp.(img.grad |> cpu, 0.0, 1.0), prob[i], idx))
-    img.grad .= 0.0
-  end
-  grads
-end
-
+function Deconvolution(model::Chain)
+  change_activation!(model, guided_relu2)
+  Backprop(model[1:end-1] |> gpu)
 end
 
 #------------------------Grad CAM--------------------------
@@ -135,11 +95,8 @@ end
 #----------------------Usage Notes-------------------------
 
 # img = reshape(normalize(im2arr(load("cat_dog.png"))) * 255, 224, 224, 3, 1);
-# img = param(img) |> gpu;
-# model = model.layers[1:end-1]
-# preds = backpropagation(img);
-# probs = softmax(preds);
-# prob, inds = get_topk(probs.data)
-# ∇backpropagation(preds, inds[1])
-# x = permutedims(reshape(clamp.(img.grad, 0.0, 1.0), 224, 224, 3), (3, 2, 1)) |> cpu;
+# m = VanillaBackprop(model.layers)
+# m(img)
+# x = permutedims(reshape(m[1][1], 224, 224, 3), (3, 2, 1));
 # colorview(RGB{eltype(x)}, x)
+# Gray.(colorview(RGB{eltype(x)}, x))
